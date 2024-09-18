a710033242e0c5e553c396560104fb0d18df81ab
Rob Gordon <rob.gordon@tone-row.com>
2024-09-18 15:38:58 -0400

    Add Create New

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,7 @@
 import { Banner } from "@/components/Banner";
 import { getIndexPolls } from "@/lib/getIndexPolls";
 import Link from "next/link";
-import { FiCalendar } from "react-icons/fi";
+import { FiCalendar, FiPlus } from "react-icons/fi";
 
 export default async function Home() {
   const polls = await getIndexPolls();
@@ -15,6 +15,7 @@ export default async function Home() {
         Latest Polls
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 content-start">
+        <CreatePoll />
         {polls.map((poll) => (
           <Poll key={poll.id} {...poll} />
         ))}
@@ -59,6 +60,29 @@ function Poll({
         <span className="text-sm text-neutral-700 dark:text-neutral-300">
           {author_name}
         </span>
+      </div>
+    </Link>
+  );
+}
+
+function CreatePoll() {
+  return (
+    <Link
+      href="/create"
+      className="bg-neutral-100 dark:bg-neutral-800 rounded-lg p-6 grid content-between min-h-[160px] lg:min-h-[200px] shadow-md hover:rotate-1 hover:scale-105 transition-all duration-300"
+    >
+      <div className="grid gap-2">
+        <h2 className="text-xl font-bold leading-tight text-neutral-700 dark:text-neutral-100">
+          Create your own poll!
+        </h2>
+        <p className="text-sm text-neutral-600 dark:text-neutral-300 text-pretty">
+          It only takes a few minutes to create a poll, and you can create
+          public or private polls.
+        </p>
+      </div>
+      <div className="flex items-center gap-2 font-medium">
+        <FiPlus className="w-5 h-5" />
+        Create poll
       </div>
     </Link>
   );
