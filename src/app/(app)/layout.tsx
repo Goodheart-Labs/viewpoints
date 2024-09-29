802e5ed9acb4576d6a5d85227c0483f575f9a093
Rob Gordon <rob.gordon@tone-row.com>
2024-09-29 09:34:47 -0400

    Add Posthog

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -10,6 +10,8 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryProvider } from "@/components/QueryProvider";
 import { SEO } from "@/lib/copy";
 import { getBaseUrl } from "@/lib/getBaseUrl";
+import { PosthogProvider } from "@/components/PosthogProvider";
+import { TrackPageView } from "@/components/TrackPageView";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -36,37 +38,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        variables: { colorPrimary: "#000000" },
-        elements: {
-          avatarBox: "w-6 h-6",
-        },
-      }}
-    >
-      <QueryProvider>
-        <html lang="en" suppressHydrationWarning>
-          <body
-            className={cn(
-              inter.variable,
-              "bg-neutral-50 text-foreground dark:bg-foreground dark:text-background antialiased",
-              "grid grid-rows-[auto_minmax(0,1fr)_auto] min-h-[100dvh]",
-            )}
-          >
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
+    <PosthogProvider>
+      <ClerkProvider
+        appearance={{
+          variables: { colorPrimary: "#000000" },
+          elements: {
+            avatarBox: "w-6 h-6",
+          },
+        }}
+      >
+        <QueryProvider>
+          <html lang="en" suppressHydrationWarning>
+            <body
+              className={cn(
+                inter.variable,
+                "bg-neutral-50 text-foreground dark:bg-foreground dark:text-background antialiased",
+                "grid grid-rows-[auto_minmax(0,1fr)_auto] min-h-[100dvh]",
+              )}
             >
-              <Header />
-              {children}
-              <Footer />
-              <Toaster />
-            </ThemeProvider>
-          </body>
-        </html>
-      </QueryProvider>
-    </ClerkProvider>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                <Header />
+                {children}
+                <Footer />
+                <Toaster />
+              </ThemeProvider>
+              <TrackPageView />
+            </body>
+          </html>
+        </QueryProvider>
+      </ClerkProvider>
+    </PosthogProvider>
   );
 }
